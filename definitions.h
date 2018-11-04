53d001d0f89f9d54f58ab9ad1a5264acfeefd3a4
lisasveta <svgorbachenko@gmail.com>
2018-11-04 12:16:04 +0200

    added changes to enumeration members names

--- definitions.h
+++ definitions.h
@@ -1,8 +1,9 @@
 #pragma once
+
 struct Interval
 {
 	int m_nBeg;
 	int m_nEnd;
 };
 
-enum ErrCode {m_eWRITE_OK, m_eWRITE_FAILED, m_eREAD_OK, m_eREAD_FILED, m_eFILE_NOT_EXIST, m_eFILE_IS_EMPTY, m_eFILE_NO_ACCESS, m_eMEM_BAD_ALLOC};
+enum ErrCode {WRITE_OK, WRITE_FAILED, READ_OK, READ_FILED, FILE_NOT_EXIST, FILE_IS_EMPTY, FILE_NO_ACCESS, MEM_BAD_ALLOC};

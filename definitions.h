3e51fe234d85d473f45799a7171100371b5391c5
lisasveta <svgorbachenko@gmail.com>
2018-11-10 20:40:57 +0200

    file with definitions enumeration and structure Interval

--- definitions.h
+++ definitions.h
@@ -1,9 +1,13 @@
-#pragma once
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
 
 struct Interval
 {
-	int m_nBeg;
-	int m_nEnd;
+    int m_nBeg;
+    int m_nEnd;
 };
 
-enum ErrCode {WRITE_OK, WRITE_FAILED, READ_OK, READ_FILED, FILE_NOT_EXIST, FILE_IS_EMPTY, FILE_NO_ACCESS, MEM_BAD_ALLOC};
+enum ErrCode {READ_OK, READ_FAILED, FILE_NOT_EXIST, FILE_NO_ACCESS, WRITE_OK, WRITE_FAILED, BAD_ALLOC, NO_DATA_TO_SAVE};
+
+#endif // DEFINITIONS_H
+

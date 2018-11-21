e9c189309405381289a5f2f3529446f3d8472026
lisasveta <svgorbachenko@gmail.com>
2018-11-21 09:57:48 +0200

    added code for communication with user

--- main.cpp
+++ main.cpp
@@ -3,12 +3,9 @@
 #include "DataReaderFromXML.h"
 #include "PrimeNumbers.h"
 #include "DataSaverToXML.h"
-//#include "definitions.h"
-using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
     PrimeNumbers prNumb;
     DataReaderFromXML reader("/home/ai/PrimeNumbers/intervals.xml");
     ErrCode er = prNumb.readData(&reader);
@@ -18,7 +15,37 @@ int main()
         prNumb.showData();
         DataSaverToXML saver("result.xml");
         ErrCode er = prNumb.saveData(&saver);
+        switch(er)
+        {
+        case WRITE_FAILED:
+            std::cout << "error: file was fail in saving" << std::endl;
+            break;
+        case WRITE_OK:
+            std::cout << "file was saved successfuly" << std::endl;
+            break;
+        }
         std::cout << er << std::endl;
+    }
+    else
+    {
+        switch(er)
+        {
+        case READ_FAILED:
+            std::cout << "error: file was fail in reading" << std::endl;
+            break;
+        case FILE_NOT_EXIST:
+            std::cout << "error: file don't exist" << std::endl;
+            break;
+        case FILE_NO_ACCESS:
+            std::cout << "error: no access to file" << std::endl;
+            break;
+        case BAD_ALLOC:
+            std::cout << "error: bad alloc" << std::endl;
+            break;
+        default:
+            std::cout << "error: undefined error" << std::endl;
+            break;
+        }
     }
     return 0;
 }

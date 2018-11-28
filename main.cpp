9f10b143f0a35d5c18826cafb041f599420d25b0
lisasveta <svgorbachenko@gmail.com>
2018-11-28 22:28:33 +0200

    added code for dynamic creating reader object

--- main.cpp
+++ main.cpp
@@ -1,61 +1,76 @@
 ﻿#include <iostream>
-#include "DataReader.h"
+#include "BaseDataReader.h"
 #include "DataReaderFromXML.h"
 #include "PrimeNumbers.h"
 #include "DataSaverToXML.h"
-#include <sstream>
+#include <string>
 
 int main()
 {
-    std::stringstream ss("somefirstnum 45 theanothenumber 67 anotherintnumber");
-    char str1[100], str2[100];
-    int nNum1, nNum2;
-    ss >> str1 >> nNum1 >> str2 >> nNum2;
-    std::cout << str1 << std::endl << nNum1 << std::endl << str2 << std::endl << nNum2 << std::endl;
-
-    return 0;
     PrimeNumbers prNumb;
-    DataReaderFromXML reader;
-    reader.setFileName("/home/ai/PrimeNumbers/intervals.xml");
-    ErrCode er = prNumb.readData(&reader);
-    if(READ_OK == er)
-    {
-        prNumb.parseData();
-        prNumb.showData();
-        DataSaverToXML saver("result.xml");
-        ErrCode er = prNumb.saveData(&saver);
-        switch(er)
-        {
-        case WRITE_FAILED:
-            std::cout << "error: file was fail in saving" << std::endl;
-            break;
-        case WRITE_OK:
-            std::cout << "file was saved successfuly" << std::endl;
-            break;
-        }
-        std::cout << er << std::endl;
-    }
-    else
-    {
-        switch(er)
-        {
-        case READ_FAILED:
-            std::cout << "error: file was fail in reading" << std::endl;
-            break;
-        case FILE_NOT_EXIST:
-            std::cout << "error: file don't exist" << std::endl;
-            break;
-        case FILE_NO_ACCESS:
-            std::cout << "error: no access to file" << std::endl;
-            break;
-        case BAD_ALLOC:
-            std::cout << "error: bad alloc" << std::endl;
-            break;
-        default:
-            std::cout << "error: undefined error" << std::endl;
-            break;
-        }
-    }
-    return 0;
-}
+    //menu
+    int nChoise;
+    std::cout << "Enter " << XML_FILE << " to read numbers from xml file" << std::endl;
+    //and so on...
 
+    std::cin >> nChoise;
+    switch(nChoise)
+    {
+    case XML_FILE:
+    {
+        DataReaderFromXML* pReader = new DataReaderFromXML();
+        // gets file name from user...
+        pReader->setFileName("/home/ai/PrimeNumbers/intervals.xml");
+        ErrCodeRead er = prNumb.readData(pReader); //client hasn't setReader function
+        //it gives reader object as a parameter of
+        //ErrCodeRead readData(BaseDataReader* pReader); function
+        if(READ_OK == er)
+        {
+            prNumb.parseData();
+            DataSaverToXML saver;
+            saver.setFileName("result.xml");
+            ErrCodeWrite er = prNumb.saveData(&saver);
+            switch(er)
+            {
+            case WRITE_FAILED:
+                std::cout << "file wasn't saved" << std::endl;
+                break;
+            case WRITE_OK:
+                std::cout << "file was saved successfuly" << std::endl;
+                break;
+            case NO_DATA_TO_SAVE:
+                std::cout << "no data to save in file" << std::endl;
+                break;
+            }
+        }
+        else
+        {
+            switch(er)
+            {
+            case READ_FAILED:
+                std::cout << "error: file was fail in reading" << std::endl;
+                break;
+            case FILE_NOT_EXIST:
+                std::cout << "error: file don't exist" << std::endl;
+                break;
+            case FILE_NO_ACCESS:
+                std::cout << "error: no access to file" << std::endl;
+                break;
+            case BAD_ALLOC_READ:
+                std::cout << "error: bad alloc" << std::endl;
+                break;
+            default:
+                std::cout << "error: undefined error" << std::endl;
+                break;
+            }
+        }
+    }
+        break;
+
+    //todo: implement for file with other format or other source of data
+    }
+
+
+
+   return 0;
+}

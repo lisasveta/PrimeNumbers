c1ea7e84ca65c25492da511786432f6aeb7134c0
lisasveta <svgorbachenko@gmail.com>
2018-11-28 19:26:25 +0200

    syntactic changes

--- PrimeNumbers.cpp
+++ PrimeNumbers.cpp
@@ -1,11 +1,8 @@
 #include "PrimeNumbers.h"
 #include <thread>
+#include <sstream>
 
 PrimeNumbers::PrimeNumbers():m_pSaver(nullptr), m_pReader(nullptr)
-{
-}
-
-PrimeNumbers::~PrimeNumbers()
 {
 }
 
@@ -15,7 +12,7 @@ bool PrimeNumbers::isPrimeNumber(int n)
     int nMaxDelimiter = n/2;
     for(int i = 2; i < nMaxDelimiter; ++i)
     {
-        if(n % nMaxDelimiter == 0)
+        if(0 == n % nMaxDelimiter)
         {
             bPrime = false;
             break;
@@ -24,23 +21,23 @@ bool PrimeNumbers::isPrimeNumber(int n)
     return bPrime;
 }
 
-void PrimeNumbers::threadFunction(dataToThread *pData)
+void PrimeNumbers::threadFunction(dataToThread* pData)
 {
-    std::lock_guard<std::mutex> guard(*pData->m_pMutex);
     for(int i = pData->m_Interval.m_nBeg; i <= pData->m_Interval.m_nEnd; ++i)
     {
         if(isPrimeNumber(i))
         {
+            std::lock_guard<std::mutex> guard(*pData->m_pMutex);
             m_primeNumbers.insert(i);
         }
     }
     delete pData;
 }
 
-ErrCode PrimeNumbers::readData(DataReader *pReader)
+ErrCodeRead PrimeNumbers::readData(BaseDataReader* pReader)
 {
     m_pReader = pReader;
-    ErrCode err = m_pReader->readData();
+    ErrCodeRead err = m_pReader->readData();
     return err;
 }
 
@@ -49,15 +46,15 @@ void PrimeNumbers::parseData()
     if(READ_OK == m_pReader->getReadStatus())
     {
        m_pReader->parseData();
-       std::vector<Interval> data = m_pReader->getIntervals();
+       std::vector< Interval > data = m_pReader->getIntervals();
        unsigned nIntervalsNumber = static_cast<unsigned>(data.size());
        if(0 != nIntervalsNumber)
        {
            std::mutex mut;
-           std::thread **pThreads = new std::thread *[nIntervalsNumber];
+           std::thread** pThreads = new std::thread *[nIntervalsNumber];
            for(unsigned i = 0; i < nIntervalsNumber; ++i)
            {
-                dataToThread  *pDataToThread = new dataToThread(data[i], &mut, m_primeNumbers);
+                dataToThread* pDataToThread = new dataToThread(data[i], &mut, m_primeNumbers);
                 pThreads[i] = new std::thread(&PrimeNumbers::threadFunction, this, pDataToThread);
 
            }
@@ -75,34 +72,29 @@ void PrimeNumbers::parseData()
     }
 }
 
-ErrCode PrimeNumbers::saveData(DataSaver *pSaver)
+ErrCodeWrite PrimeNumbers::saveData(BaseDataSaver* pSaver)
 {
-    ErrCode err = WRITE_OK;
+    ErrCodeWrite err = WRITE_OK;
     m_pSaver = pSaver;
-    std::string dataToSave;
-    dataToSave.append("<root>\n<primes>");
-    std::set<int>::iterator itBeg = m_primeNumbers.begin();
-    std::set<int>::iterator itEnd = m_primeNumbers.end();
-    for(std::set<int>::iterator it = itBeg; it != itEnd; ++it)
+    std::stringstream streamData;
+    streamData << "<root>\n<primes>";
+    std::set< int >::iterator itBeg = m_primeNumbers.begin();
+    std::set< int >::iterator itEnd = m_primeNumbers.end();
+    for(std::set< int >::iterator it = itBeg; it != itEnd; ++it)
     {
-        dataToSave.append(std::to_string(*it));
-        dataToSave.append(" ");
+        streamData << std::to_string(*it) << " ";
     }
-    dataToSave.append("</primes>\n</root>");
-    std::cout << dataToSave << std::endl;
-    err = m_pSaver->saveData(dataToSave);
+
+    streamData << "</primes>\n</root>";
+    err = m_pSaver->saveData(streamData.str());
     return err;
 }
 
-std::set<int> PrimeNumbers::getData()const
-{
-    return m_primeNumbers;
-}
 void PrimeNumbers::showData()
 {
-    std::set<int>::iterator itBeg = m_primeNumbers.begin();
-    std::set<int>::iterator itEnd = m_primeNumbers.end();
-    for(std::set<int>::iterator it = itBeg; it != itEnd; ++it)
+    std::set< int >::iterator itBeg = m_primeNumbers.begin();
+    std::set< int >::iterator itEnd = m_primeNumbers.end();
+    for(std::set< int >::iterator it = itBeg; it != itEnd; ++it)
     {
         std::cout << *it << " ";
     }
--- PrimeNumbers.h
+++ PrimeNumbers.h
@@ -1,35 +1,33 @@
 #ifndef PRIMENUMBERS_H
 #define PRIMENUMBERS_H
-#include "DataSaver.h"
-#include "DataReader.h"
+#include "BaseDataSaver.h"
+#include "BaseDataReader.h"
 #include <set>
 #include <mutex>
 
 struct dataToThread
 {
     Interval m_Interval;
-    std::mutex * m_pMutex;
-    std::set<int> & m_PrimeNumbers;
+    std::mutex* m_pMutex;
+    std::set< int >& m_PrimeNumbers;
 
-    dataToThread(Interval interval, std::mutex *pMutex, std::set<int>& primeNumbers):m_Interval(interval), m_pMutex(pMutex), m_PrimeNumbers(primeNumbers){}
+    dataToThread(Interval interval, std::mutex* pMutex, std::set< int >& primeNumbers):m_Interval(interval), m_pMutex(pMutex), m_PrimeNumbers(primeNumbers){}
 };
 
 class PrimeNumbers
 {
-    std::set<int> m_primeNumbers;
-    DataSaver * m_pSaver;
-    DataReader * m_pReader;
-    void threadFunction(dataToThread *pData);
+    std::set< int > m_primeNumbers;
+    BaseDataSaver* m_pSaver;
+    BaseDataReader* m_pReader;
+    void threadFunction(dataToThread* pData);
     bool isPrimeNumber(int n);
 
 public:
     PrimeNumbers();
-    ~PrimeNumbers();
 
-    ErrCode readData(DataReader *pReader);
+    ErrCodeRead readData(BaseDataReader* pReader);
     void parseData();
-    ErrCode saveData(DataSaver *pSaver);
-    std::set<int> getData()const;
+    ErrCodeWrite saveData(BaseDataSaver* pSaver);
     void showData();
 };
 
